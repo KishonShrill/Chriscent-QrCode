e560b8c972d3d79678425dd9231ff0549c72fa23
KishonShrill <crystalbluew@gmail.com>
2023-10-25 08:43:44 +0800

    QrScanner works: next step, connecting to mongoDB

--- src/components/QRCodeScanner.jsx
+++ src/components/QRCodeScanner.jsx
@@ -1,26 +1,68 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState, forwardRef } from 'react';
 import { QrReader } from 'react-qr-reader';
 
-function QRCodeScanner(props) {
-    const [data, setData] = useState('No result');
-  
-    return (
-      <>
+const QRCodeScanner = forwardRef((props, ref) => {
+  const [data, setData] = useState('No result');
+
+  const initialFacing = localStorage.getItem('facing') || 'environment';
+  const [facing, setFacing] = useState(initialFacing); // 'user' or 'environment'
+  const videoRef = ref || useRef(null);
+
+  const handleSwitchCamera = () => {
+    // Toggle the 'facing' state.
+    const newFacing = facing === 'environment' ? 'user' : 'environment';
+    setFacing(newFacing);
+
+    // Save the updated 'facing' value to local storage.
+    localStorage.setItem('facing', newFacing);
+
+    // Refresh the browser to apply the new camera facing mode.
+    window.location.reload();
+  };  
+
+  return (
+    <>
+      {facing === 'environment' ? (
+        <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+        ref={videoRef}
+        constraints={{
+          facingMode: 'environment'
+        }}
+      />
+      ) : (
         <QrReader
           onResult={(result, error) => {
             if (!!result) {
               setData(result?.text);
             }
-  
+
             if (!!error) {
               console.info(error);
             }
           }}
           style={{ width: '100%' }}
+          ref={videoRef}
+          constraints={{
+            facingMode: 'user'
+          }}
         />
-        <p>{data}</p>
-      </>
-    );
-  };
+      )}
+
+      {/* TODO: Connect to mongoDB to display data from qr code */}
+      <p className='qrReader__data'>{data}</p>
+      <button className='qrReader__camSwitch' onClick={handleSwitchCamera}>Switch Camera</button>
+    </>
+  );
+});
 
 export default QRCodeScanner;
